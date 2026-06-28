4a3c14724bad14202086149b57dd30c2bbd178c7
anthonybistocchi-txt <anthonybistocchi.txt@gmail.com>
2026-06-27 23:04:53 -0300

    feat: wire NestJS modules and global validation pipe

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './database/database.config';
+import { NfeModule } from './nfe/nfe.module';
+import { PaymentsModule } from './payment-gateway/payment-gateway.module';
 
 @Module({
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.getOrThrow('database'),
+    }),
+    PaymentsModule,
+    NfeModule,
+  ],
 })
 export class AppModule {}
